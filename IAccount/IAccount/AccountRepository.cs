82ba2c939ff59d25df51f173746bd07bf99e9548
Magdalena Witkowska <mawitkow@poczta.onet.pl>
2014-04-23 21:33:03 +0200

    Basic program + simple interface added

---
+++ IAccount/IAccount/AccountRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+
+namespace AccountRepository
+{
+    interface AccountRepository
+    {
+        Account getAccount(String number, int type);
+        Account createAccount(int type);
+    }
+}
